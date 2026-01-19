dbcc02d1be52b8cbf7c73f4b85670d9350e8840e
Aram Hovhannisyan <araminbox@gmail.com>
2026-01-19 18:53:43 +0000

    Added version 2 for `fct_orders`; Edited Readme

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+
+with orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+items as (
+    select * from {{ ref('stg_items') }}
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(['o.order_id', 'i.item_id']) }} as order_item_key,
+
+    -- natural foreign keys
+    o.order_id,
+    o.customer_id,
+    o.store_id,
+    i.sku as product_sku,
+
+    -- timestamps
+    o.ordered_at,
+
+    -- measures
+    o.subtotal,
+    o.tax_paid,
+    o.order_total as order_amount
+
+from orders o
+join items i
+  on o.order_id = i.order_id
